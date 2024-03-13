7f87e39d41b8996db5adfb3f28b620c68b6a5619
minji <qkralswl6125@naver.com>
2024-03-13 16:44:28 +0900

    feat: Level1 구현

--- src/main/java/backend/likelion/todos/member/MemberRepository.java
+++ src/main/java/backend/likelion/todos/member/MemberRepository.java
@@ -16,25 +16,30 @@ public class MemberRepository {
         // TODO [1단계] member의 id를 설정하세요.
         // TODO [1단계] members 맵에 member를 추가하세요.
         // TODO [1단계] member를 반환하세요.
-        return null;
+        member.setId(id);
+        members.put(id++, member);
+        return member;
     }
 
     // 주어진 id에 해당하는 멤버를 찾아 Optional로 반환합니다.
     public Optional<Member> findById(Long id) {
         // TODO [1단계] id를 이용하여 members 맵에서 멤버를 찾으세요.
         // TODO [1단계] 찾은 멤버를 Optional로 감싸서 반환하세요.
-        return null;
+        return Optional.ofNullable(members.get(id));
     }
 
     // 주어진 username과 일치하는 멤버를 찾아 Optional로 반환합니다.
     public Optional<Member> findByUsername(String username) {
         // TODO [1단계] members 맵에서 username이 일치하는 멤버를 스트림을 사용해 찾으세요.
         // TODO [1단계] 찾은 멤버를 Optional로 감싸서 반환하세요.
-        return null;
+        return members.values().stream()
+                .filter(member -> username.equals(member.getUsername()))
+                .findAny();
     }
 
     // 저장소의 모든 멤버를 제거합니다.
     public void clear() {
         // TODO [1단계] members 맵의 모든 내용을 제거하세요.
+        members.clear();
     }
 }
