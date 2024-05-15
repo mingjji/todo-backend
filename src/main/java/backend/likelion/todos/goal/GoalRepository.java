9fa4e8b267a868a3221b9a5e3fba07992a192380
minji <qkralswl6125@naver.com>
2024-05-15 17:12:57 +0900

    feat: 'Goal' 엔티티 및 리포지토리를 JPA를 사용하여 구현

--- src/main/java/backend/likelion/todos/goal/GoalRepository.java
+++ src/main/java/backend/likelion/todos/goal/GoalRepository.java
@@ -1,17 +1,15 @@
 package backend.likelion.todos.goal;
 
 import java.util.List;
+
+import backend.likelion.todos.member.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GoalRepository {
+public interface GoalRepository extends JpaRepository<Member, Long> {
     // TODO [10단계] JpaRepository를 상속받습니다.
 
     // TODO [10단계] 아래는 기존 코드입니다. 컴파일 오류가 발생하고 있는데, 이를 해결하세요.
-    public List<Goal> findAllByMemberId(Long memberId) {
-        return goals.values()
-                .stream()
-                .filter(it -> it.getMember().getId().equals(memberId))
-                .toList();
-    }
+    List<Goal> findAllByMemberId(Long memberId);
 }
