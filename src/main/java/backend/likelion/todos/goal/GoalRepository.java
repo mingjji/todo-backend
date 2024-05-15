8efdb7361b0006c77f3b4cf252532b25216e4d10
minji <qkralswl6125@naver.com>
2024-05-15 17:28:35 +0900

    fix: GoalRepository 엔티티 타입 지정 수정

--- src/main/java/backend/likelion/todos/goal/GoalRepository.java
+++ src/main/java/backend/likelion/todos/goal/GoalRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface GoalRepository extends JpaRepository<Member, Long> {
+public interface GoalRepository extends JpaRepository<Goal, Long> {
     // TODO [10단계] JpaRepository를 상속받습니다.
 
     // TODO [10단계] 아래는 기존 코드입니다. 컴파일 오류가 발생하고 있는데, 이를 해결하세요.
